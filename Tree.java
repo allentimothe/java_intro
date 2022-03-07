17070294e337a5d76fe60d3e5fd0c860c447f556
Allen Timothe <allen.timothe@gmail.com>
2022-03-07 08:53:37 -0500

    Lesson update

---
+++ Tree.java
@@ -0,0 +1,18 @@
+public class Tree{ 
+  public static void main(String[] args){
+    // I'm Allen and I'm looking to print Trees
+    System.out.println("Hey there, I'm Allen and I'm learning to code Java!");
+    System.out.println("I'm going to plant a tree today");
+    System.out.println("     *  ");
+    System.out.println("    *** ");
+    System.out.println("   ***** ");
+    System.out.println("  *******  ");
+    System.out.println(" *********  ");   
+    System.out.println("***********  ");
+    System.out.println("    *** ");
+    System.out.println("    *** ");
+    System.out.println("    ***  ");
+    System.out.println("    ***  ");   
+    System.out.println("  #######  ");    
+  }
+}
