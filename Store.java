afb0c764667889fc7c1e4fa6e96d1105035d702a
Allen Timothe <allen.timothe@gmail.com>
2022-03-08 21:18:06 -0500

    Most Recent Projects

--- Store.java
+++ Store.java
@@ -22,3 +22,47 @@ public class Store {
     System.out.println(lemonadeStand.price);
   }
 }
+
+//------------------------------
+// Returns
+
+// Remember, variables can only exist in the scope that they were declared in. We can use a value outside of the method it was created in if we return it from the method.
+
+// We return a value by using the keyword return:
+
+// public int numberOfTires() {
+//    int tires = 4;
+//    // return statement
+//    return tires;
+// }
+//-----------------------------------
+// public class Store {
+//   // instance fields
+//   String productType;
+//   double price;
+  
+//   // constructor method
+//   public Store(String product, double initialPrice) {
+//     productType = product;
+//     price = initialPrice;
+//   }
+  
+//   // increase price method
+//   public void increasePrice(double priceToAdd){
+//     double newPrice = price + priceToAdd;
+//     price = newPrice;
+//   }
+  
+//   // get price with tax method
+//   public double getPriceWithTax(){
+//     double totalPrice = price + price * 0.08;
+//     return totalPrice;
+//   }
+
+//   // main method
+//   public static void main(String[] args) {
+//     Store lemonadeStand = new Store("Lemonade", 3.75);
+//     double lemonadePrice = lemonadeStand.getPriceWithTax();
+//     System.out.println(lemonadePrice);
+//   }
+// }
